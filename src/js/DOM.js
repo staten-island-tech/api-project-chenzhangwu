1d6b172b7aa8e984cc5932f535ab16dba36c2c04
wilsonw13 <71456315+wilsonw13@users.noreply.github.com>
2021-05-18 11:46:00 -0400

    test
    - still figuring out how to use fetchAPI for POST
    - reorganized the data into a better table

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -1,5 +1,6 @@
 const DOMSelectors = {
   container: document.querySelector(".container"),
+  table: document.querySelector(".table"),
 };
 
 export { DOMSelectors };
--- src/js/index.js
+++ src/js/index.js
@@ -3,21 +3,43 @@ import { DOMSelectors } from "./DOM";
 const query = async function () {
   try {
     const response = await fetch(
-      "https://api.spacexdata.com/v4/launches/upcoming"
+      "https://api.spacexdata.com/v4/launches/query",
+      {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        body: {
+          query: {
+            $text: {
+              $search: "2",
+            },
+          },
+          options: {
+            sort: {
+              flight_number: "asc",
+            },
+            limit: 50,
+            populate: ["rocket"],
+          },
+        },
+      }
     );
     const launchData = await response.json();
-    launchData.forEach((launch) => {
-      DOMSelectors.container.insertAdjacentHTML(
+    console.log(launchData.docs);
+    launchData.docs.forEach((launch) => {
+      DOMSelectors.table.insertAdjacentHTML(
         "beforeend",
-        `<div class="launch-item">
-        <h3 class="launch-name">${launch.name}</h3>
-        <h3 class="launch-time">${launch.date_utc}</h3>
-      </div>`
+        `<tr>
+        <td><img src="${launch.links.patch.small}" alt="Patch"></td>
+        <td>${launch.flight_number}</td>
+        <td>${launch.name}</td>
+        <td>${launch.date_utc}</td>
+        <td>${launch.rocket}</td>
+      </tr>`
       );
+      console.log(launch.data_utc);
     });
   } catch (error) {
     console.log(error);
-    alert("Great Error Handling :D - An error as occured!");
+    // alert("Great Error Handling :D - An error as occured!");
   }
 };
 query();
